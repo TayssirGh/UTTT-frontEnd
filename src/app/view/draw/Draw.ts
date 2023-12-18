a46e2d6932d783afff9360a270deec365ff7bb13
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2023-12-18 14:46:50 -0500

    [ADDED] canvas setup

---
+++ src/app/view/draw/Draw.ts
@@ -0,0 +1,11 @@
+import {BigBoard} from "../../model/BigBoard";
+
+export class Draw{
+  private WIDTH = 800;
+  private HEIGHT = 850;
+
+  private COLORS = {white: "rgb(254, 250, 236)", black: "rgb(7, 5, 14)", blue: "rgb(36, 32, 95)", red: "rgb(129, 43, 56)"};
+  public drawBoard(ctx: CanvasRenderingContext2D, model: BigBoard){
+
+  }
+}
--- src/app/view/ttt/ttt.component.ts
+++ src/app/view/ttt/ttt.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {BigBoard} from "../../model/BigBoard";
+import {Draw} from "../draw/Draw";
 
 @Component({
   selector: 'app-ttt',
   templateUrl: './ttt.component.html',
   styleUrl: './ttt.component.css'
 })
-export class TttComponent {
-  board = new BigBoard();
-  public click(){
-    console.log(this.board.boards[0][0].board[0][1])
+export class TttComponent implements OnInit{
+  @ViewChild('canvasElt', {static : true}) canvas : ElementRef<HTMLCanvasElement>
+  private ctx: CanvasRenderingContext2D ;
+  private model : BigBoard = new BigBoard();
+  private draw : Draw = new Draw();
+
+  ngOnInit(): void {
+    this.ctx = this.canvas?.nativeElement.getContext('2d');
+    if (this.ctx) {
+      this.draw.drawBoard(this.ctx, this.model);
+    } else {
+      console.error('Failed to get canvas context!');
+    }
   }
+
+
 }
