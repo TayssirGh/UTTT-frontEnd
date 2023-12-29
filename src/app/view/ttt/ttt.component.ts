74d19c8a7925493d50ba5e7bc860db85aa74c6f4
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2023-12-28 21:51:37 -0500

    [FIXED] player position

--- src/app/view/ttt/ttt.component.ts
+++ src/app/view/ttt/ttt.component.ts
@@ -68,34 +68,6 @@ export class TttComponent implements OnInit{
         }
       }
     }
-    // if(!this.draw.gameStart){
-    //   this.click = !this.click;
-    //   this.model.boards[parseInt(s[0])][parseInt(s[1])].board[parseInt(s[2])][parseInt(s[3])] = this.changePlayer(this.click);
-    //   this.draw.enableRow = parseInt(s[2]);
-    //   this.draw.enableCol = parseInt(s[3]);
-    //   this.draw.gameStart = true
-    //   this.draw.drawBoard(this.ctx, this.model)
-    // }
-    // else if(this.model.boards[parseInt(s[0])][parseInt(s[1])].board[parseInt(s[2])][parseInt(s[3])] == State.NULL
-    //   && this.model.boards[parseInt(s[0])][parseInt(s[1])].winner == State.NULL
-    // ){
-    //   this.click = !this.click;
-    //
-    //   if( parseInt(s[0])==this.draw.enableRow && parseInt(s[1])==this.draw.enableCol){
-    //     this.draw.enableRow = parseInt(s[2]);
-    //     this.draw.enableCol = parseInt(s[3]);
-    //     // this.model.boards[parseInt(s[0])][parseInt(s[1])].board[parseInt(s[2])][parseInt(s[3])] = this.changePlayer(this.click);
-    //     this.model.boards[parseInt(s[0])][parseInt(s[1])].winner = this.changePlayer(this.click)
-    //     this.draw.drawBoard(this.ctx, this.model);
-    //   }
-    //   else {
-    //     this.draw.enableRow = parseInt(s[2]);
-    //     this.draw.enableCol = parseInt(s[3]);
-    //     this.model.boards[parseInt(s[0])][parseInt(s[1])].board[parseInt(s[2])][parseInt(s[3])] = this.changePlayer(this.click);
-    //     this.draw.drawBoard(this.ctx, this.model);
-    //
-    //   }
-    // }
 
 
   }
